9252980806b22240a715f4cb2d9cce82a5a9bda4
garohvip <garohvip@mail.ru>
2022-11-07 22:22:34 +0200

    func

--- start.py
+++ start.py
@@ -5,17 +5,17 @@ try:
         host="localhost",
         port=3306,
         user='root',
-        password='Andrik89@',  # вставить свой пароль
+        password='DataBase0321',  # вставить свой пароль
         database='musei',
         cursorclass=pymysql.cursors.DictCursor
     )
     print("Welcome to the Database!")
     with connection.cursor() as cursor:
-        create_table = "CREATE SCHEMA `musei`;"
-        cursor.execute(create_table)
+        # create_table = "CREATE SCHEMA `musei`;"
+        # cursor.execute(create_table)
         create_table = "CREATE TABLE `users` (idUser INT AUTO_INCREMENT, name varchar(30), login varchar(30) UNIQUE, password varchar(30), PRIMARY KEY (idUser));"
         cursor.execute(create_table)
-        create_table = "CREATE TABLE `exhibit` (idEx INT AUTO_INCREMENT, nameExhibit varchar(30) UNIQUE, year INT, description varchar(255), PRIMARY KEY (idEx));"
+        create_table = "CREATE TABLE `exhibit` (idEx INT AUTO_INCREMENT, nameExhibit varchar(30) UNIQUE, year INT, criterion ENUM('до нашей эры', 'наша эра'), description varchar(255), PRIMARY KEY (idEx));"
         cursor.execute(create_table)
         create_table = "CREATE TABLE `userex` (idPer INT AUTO_INCREMENT, loginPerson varchar(30), " \
                        "nameExhibit varchar(30), PRIMARY KEY (idPer), " \
