381f2f424d2530c80b80eba926d142059813716f
garohvip <garohvip@mail.ru>
2022-11-07 19:47:48 +0200

    start

--- start.py
+++ start.py
@@ -11,6 +11,8 @@ try:
     )
     print("Welcome to the Database!")
     with connection.cursor() as cursor:
+        create_table = "CREATE SCHEMA `musei`;"
+        cursor.execute(create_table)
         create_table = "CREATE TABLE `users` (idUser INT AUTO_INCREMENT, name varchar(30), login varchar(30) UNIQUE, password varchar(30), PRIMARY KEY (idUser));"
         cursor.execute(create_table)
         create_table = "CREATE TABLE `exhibit` (idEx INT AUTO_INCREMENT, nameExhibit varchar(30) UNIQUE, year INT, description varchar(30), PRIMARY KEY (idEx));"
